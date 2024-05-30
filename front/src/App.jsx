c34a80edcab91faf2218b863320cf0edb4d65e45
xanimnuriyev1 <xanimnuriyeva07@gmail.com>
2024-05-30 20:41:40 +0200

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <audio controls autoPlay muted loop>
+      <source src='music.mp3' />
+    </audio>
+    </>
+  )
+}
+
+export default App
